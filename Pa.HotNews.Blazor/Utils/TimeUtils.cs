4c7c20f5ebfc70af49d6cacdc27c341696096831
Dai Mr <1822802785@qq.com>
2026-01-04 21:08:51 +0800

    修复BUG

--- Pa.HotNews.Blazor/Utils/TimeUtils.cs
+++ Pa.HotNews.Blazor/Utils/TimeUtils.cs
@@ -9,17 +9,13 @@ public static class TimeUtils
         var span = DateTimeOffset.UtcNow - time.Value;
 
         if (span.TotalMinutes < 1) return "刚刚";
-        if (span.TotalMinutes < 60) return $"{(int)span.TotalMinutes} 分钟前";
-        if (span.TotalHours < 24) return $"{(int)span.TotalHours} 小时前";
-        if (span.TotalDays < 30) return $"{(int)span.TotalDays} 天前";
-
-        return time.Value.ToString("yyyy-MM-dd");
+        if (span.TotalMinutes < 60) return $"{(int)span.TotalMinutes}分钟前";
+        if (span.TotalHours < 24) return $"{(int)span.TotalHours}小时前";
+        return $"{(int)span.TotalDays}天前";
     }
 
     public static string GetFullTimeString(DateTimeOffset? time)
     {
-        if (!time.HasValue) return string.Empty;
-        return time.Value.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss");
+        return time?.ToString("yyyy-MM-dd HH:mm:ss") ?? string.Empty;
     }
 }
-
